062eb6a19dab7397be7c80c9939e6f6f5ced162c
praveen3073 <pop3073@gmail.com>
2020-10-15 22:22:33 +0530

    Refactored: Generic Class to Find Max

--- main/java/com/maxgenericsproblem/MaximumUsingGenerics.java
+++ main/java/com/maxgenericsproblem/MaximumUsingGenerics.java
@@ -2,25 +2,39 @@ package com.maxgenericsproblem;
 
 import java.util.Arrays;
 
+class MaxTestInput<E extends Comparable<E>> {
+    E[] array;
+
+    public MaxTestInput(E[] array) {
+        this.array = array;
+    }
+
+    public E testMaximum() {
+        return MaximumUsingGenerics.findMax(array);
+    }
+}
+
 public class MaximumUsingGenerics {
     public static void main(String[] args) {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
         System.out.println("Welcome to Test Maximum Program");
 
         //Max of three Integers
         Integer[] intArray = new Integer[]{13, 65, 47};
-        System.out.println("Maximum interger " + Arrays.toString(intArray) + ": " + maximumUsingGenerics.findMax(intArray));
+        MaxTestInput<Integer> intInput = new MaxTestInput<>(intArray);
+        System.out.println("Maximum integer " + Arrays.toString(intArray) + ": " + intInput.testMaximum());
 
         //Max of three Floats
         Float[] floatArray = new Float[]{6.5f, 4.3f, 8.8f};
-        System.out.println("Maximum float of " + Arrays.toString(floatArray) + ": " + maximumUsingGenerics.findMax(floatArray));
+        MaxTestInput<Float> floatInput = new MaxTestInput<>(floatArray);
+        System.out.println("Maximum float of " + Arrays.toString(floatArray) + ": " + floatInput.testMaximum());
 
         //Max of three Strings
         String[] stringArray = new String[]{"Chennai", "Kolkata", "Delhi"};
-        System.out.println("Maximum string of " + Arrays.toString(stringArray) +": " + maximumUsingGenerics.findMax(stringArray));
+        MaxTestInput<String> stringInput = new MaxTestInput<>(stringArray);
+        System.out.println("Maximum string of " + Arrays.toString(stringArray) + ": " + stringInput.testMaximum());
     }
 
-    protected <E extends Comparable> E findMax(E[] array) {
+    protected static <E extends Comparable<E>> E findMax(E[] array) {
         E max = array[0].compareTo(array[1]) > 0 ? array[0] : array[1];
         return max.compareTo(array[2]) > 0 ? max : array[2];
     }
--- test/java/com/maxgenericsproblem/MaximumUsingGenericsTest.java
+++ test/java/com/maxgenericsproblem/MaximumUsingGenericsTest.java
@@ -6,64 +6,82 @@ import org.junit.Test;
 public class MaximumUsingGenericsTest {
     @Test
     public void given3Integers_WhenMaxAtIndex0_ReturnTrue() {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
-        int result = maximumUsingGenerics.findMax(new Integer[]{11, 3, 9});
+        int result = MaximumUsingGenerics.findMax(new Integer[]{11, 3, 9});
+        Assert.assertEquals(11, result);
+        MaxTestInput<Integer> intInput = new MaxTestInput<>(new Integer[]{11, 3, 9});
+        result = intInput.testMaximum();
         Assert.assertEquals(11, result);
     }
 
     @Test
     public void given3Integers_WhenMaxAtIndex1_ReturnTrue() {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
-        int result = maximumUsingGenerics.findMax(new Integer[]{3, 11, 9});
+        int result = MaximumUsingGenerics.findMax(new Integer[]{3, 11, 9});
+        Assert.assertEquals(11, result);
+        MaxTestInput<Integer> intInput = new MaxTestInput<>(new Integer[]{3, 11, 9});
+        result = intInput.testMaximum();
         Assert.assertEquals(11, result);
     }
 
     @Test
     public void given3Integers_WhenMaxAtIndex2_ReturnTrue() {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
-        int result = maximumUsingGenerics.findMax(new Integer[]{9, 3, 11});
+        int result = MaximumUsingGenerics.findMax(new Integer[]{9, 3, 11});
+        Assert.assertEquals(11, result);
+        MaxTestInput<Integer> intInput = new MaxTestInput<>(new Integer[]{9, 3, 11});
+        result = intInput.testMaximum();
         Assert.assertEquals(11, result);
     }
 
     @Test
     public void given3Floats_WhenMaxAtIndex0_ReturnTrue() {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
-        float result = maximumUsingGenerics.findMax(new Float[]{11.0f, 3.0f, 9.0f});
+        float result = MaximumUsingGenerics.findMax(new Float[]{11.0f, 3.0f, 9.0f});
+        Assert.assertEquals(11.0, result, 0.0);
+        MaxTestInput<Float> intInput = new MaxTestInput<>(new Float[]{11.0f, 3.0f, 9.0f});
+        result = intInput.testMaximum();
         Assert.assertEquals(11.0, result, 0.0);
     }
 
     @Test
     public void given3Floats_WhenMaxAtIndex1_ReturnTrue() {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
-        float result = maximumUsingGenerics.findMax(new Float[]{3.0f, 11.0f, 9.0f});
+        float result = MaximumUsingGenerics.findMax(new Float[]{3.0f, 11.0f, 9.0f});
+        Assert.assertEquals(11.0, result, 0.0);
+        MaxTestInput<Float> intInput = new MaxTestInput<>(new Float[]{3.0f, 11.0f, 9.0f});
+        result = intInput.testMaximum();
         Assert.assertEquals(11.0, result, 0.0);
     }
 
     @Test
     public void given3Floats_WhenMaxAtIndex2_ReturnTrue() {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
-        float result = maximumUsingGenerics.findMax(new Float[]{9.0f, 3.0f, 11.0f});
+        float result = MaximumUsingGenerics.findMax(new Float[]{9.0f, 3.0f, 11.0f});
+        Assert.assertEquals(11.0, result, 0.0);
+        MaxTestInput<Float> intInput = new MaxTestInput<>(new Float[]{9.0f, 3.0f, 11.0f});
+        result = intInput.testMaximum();
         Assert.assertEquals(11.0, result, 0.0);
     }
 
     @Test
     public void given3Strings_WhenMaxAtIndex0_ReturnTrue() {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
-        String result = maximumUsingGenerics.findMax(new String[]{"Peach", "Apple", "Banana"});
+        String result = MaximumUsingGenerics.findMax(new String[]{"Peach", "Apple", "Banana"});
+        Assert.assertEquals("Peach", result);
+        MaxTestInput<String> intInput = new MaxTestInput<>(new String[]{"Peach", "Apple", "Banana"});
+        result = intInput.testMaximum();
         Assert.assertEquals("Peach", result);
     }
 
     @Test
     public void given3Strings_WhenMaxAtIndex1_ReturnTrue() {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
-        String result = maximumUsingGenerics.findMax(new String[]{"Apple", "Peach", "Banana"});
+        String result = MaximumUsingGenerics.findMax(new String[]{"Apple", "Peach", "Banana"});
+        Assert.assertEquals("Peach", result);
+        MaxTestInput<String> intInput = new MaxTestInput<>(new String[]{"Apple", "Peach", "Banana"});
+        result = intInput.testMaximum();
         Assert.assertEquals("Peach", result);
     }
 
     @Test
     public void given3Strings_WhenMaxAtIndex2_ReturnTrue() {
-        MaximumUsingGenerics maximumUsingGenerics = new MaximumUsingGenerics();
-        String result = maximumUsingGenerics.findMax(new String[]{"Banana", "Apple", "Peach"});
+        String result = MaximumUsingGenerics.findMax(new String[]{"Banana", "Apple", "Peach"});
+        Assert.assertEquals("Peach", result);
+        MaxTestInput<String> intInput = new MaxTestInput<>(new String[]{"Banana", "Apple", "Peach"});
+        result = intInput.testMaximum();
         Assert.assertEquals("Peach", result);
     }
 }
