e08585a83141cdda2749f10936e9af43d15941c8
praveen3073 <pop3073@gmail.com>
2020-10-15 14:43:54 +0530

    Added: UC2 Maximum Float and Test Cases

--- main/java/com/maxgenericsproblem/MaximumUsingGenerics.java
+++ main/java/com/maxgenericsproblem/MaximumUsingGenerics.java
@@ -12,9 +12,19 @@ public class MaximumUsingGenerics {
             intArray[i] = in.nextInt();
         }
         System.out.println("Maximum interger: " + findMaxInteger(intArray));
+        System.out.println("Input 3 floats: ");
+        Float[] floatArray = new Float[3];
+        for (int i=0; i<3; i++) {
+            floatArray[i] = in.nextFloat();
+        }
+        System.out.println("Maximum float: " + findMaxFloat(floatArray));
     }
     protected static Integer findMaxInteger(Integer[] intArray) {
         Integer max = intArray[0].compareTo(intArray[1])>0 ? intArray[0] : intArray[1];
         return max.compareTo(intArray[2])>0 ? max : intArray[2];
+    }
+    protected static Float findMaxFloat(Float[] floatArray) {
+        Float max = floatArray[0].compareTo(floatArray[1])>0 ? floatArray[0] : floatArray[1];
+        return max.compareTo(floatArray[2])>0 ? max : floatArray[2];
     }
 }
--- test/java/com/maxgenericsproblem/MaximumUsingGenericsTest.java
+++ test/java/com/maxgenericsproblem/MaximumUsingGenericsTest.java
@@ -6,17 +6,37 @@ import org.junit.Test;
 public class MaximumUsingGenericsTest {
     @Test
     public void given3Integers_WhenMaxAtIndex0_ReturnTrue() {
-        int result = MaximumUsingGenerics.findMaxInteger(new Integer[]{11,3,9});
+        int result = MaximumUsingGenerics.findMaxInteger(new Integer[]{11, 3, 9});
         Assert.assertEquals(11, result);
     }
+
     @Test
     public void given3Integers_WhenMaxAtIndex1_ReturnTrue() {
-        int result = MaximumUsingGenerics.findMaxInteger(new Integer[]{3,11,9});
+        int result = MaximumUsingGenerics.findMaxInteger(new Integer[]{3, 11, 9});
         Assert.assertEquals(11, result);
     }
+
     @Test
     public void given3Integers_WhenMaxAtIndex2_ReturnTrue() {
-        int result = MaximumUsingGenerics.findMaxInteger(new Integer[]{9,3,11});
+        int result = MaximumUsingGenerics.findMaxInteger(new Integer[]{9, 3, 11});
         Assert.assertEquals(11, result);
+    }
+
+    @Test
+    public void given3Floats_WhenMaxAtIndex0_ReturnTrue() {
+        float result = MaximumUsingGenerics.findMaxFloat(new Float[]{11.0f, 3.0f, 9.0f});
+        Assert.assertEquals(11.0, result, 0.0);
+    }
+
+    @Test
+    public void given3Floats_WhenMaxAtIndex1_ReturnTrue() {
+        float result = MaximumUsingGenerics.findMaxFloat(new Float[]{3.0f, 11.0f, 9.0f});
+        Assert.assertEquals(11.0, result, 0.0);
+    }
+
+    @Test
+    public void given3Floats_WhenMaxAtIndex2_ReturnTrue() {
+        float result = MaximumUsingGenerics.findMaxFloat(new Float[]{9.0f, 3.0f, 11.0f});
+        Assert.assertEquals(11.0, result, 0.0);
     }
 }
